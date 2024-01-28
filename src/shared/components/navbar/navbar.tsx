0ce61196a670d182b49fdda291ec973bfc1a7a75
MohamedAshiq11 <ashiqap007@gmail.com>
2024-01-28 16:55:50 +0400

    feat: navbar added

--- src/shared/components/navbar/navbar.tsx
+++ src/shared/components/navbar/navbar.tsx
@@ -1,14 +1,35 @@
 import "./navbar.css";
+import { useState } from "react";
 import Global from "../../../assets/icons/navbar/index";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
-    <nav>
-      <div className="wrapper">
-        <div className="logo">
-          <span className="main">United</span>{" "}
-          <span className="sub">Industries</span>
+    <>
+      <nav>
+        <div className="wrapper">
+          <div className="logo">
+            <span className="main">United</span>{" "}
+            <span className="sub">Industries</span>
+          </div>
+          <ul>
+            <li>
+              <a href="#">Product</a>
+            </li>
+            <li>
+              <a href="#">About Us</a>
+            </li>
+            <li>
+              <a href="#">Contact Us</a>
+            </li>
+          </ul>
+          <div onClick={() => setShowMenu(true)} className="menu">
+            <img src={Global.menu} alt="" />
+          </div>
         </div>
+      </nav>
+      <div className={`mobile-nav ${showMenu ? "mobile-nav-ative" : ""}`}>
+        <img onClick={() => setShowMenu(false)} src={Global.close} alt="" />
         <ul>
           <li>
             <a href="#">Product</a>
@@ -20,11 +41,8 @@ const Navbar = () => {
             <a href="#">Contact Us</a>
           </li>
         </ul>
-        <div className="menu">
-          <img src={Global.menu} alt="" />
-        </div>
       </div>
-    </nav>
+    </>
   );
 };
 
