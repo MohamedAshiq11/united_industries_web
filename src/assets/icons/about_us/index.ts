973d145725d320174aaf05bea9b549ad5f8089a3
MohamedAshiq11 <ashiqap007@gmail.com>
2024-03-07 22:58:25 +0400

    feat:about appointment section added

---
+++ src/assets/icons/about_us/index.ts
@@ -0,0 +1,5 @@
+import aboutUsService1 from "./aboutUsService1.svg";
+import aboutUsService2 from "./aboutUsService2.svg";
+import aboutUsService3 from "./aboutUsService3.svg";
+
+export default { aboutUsService1, aboutUsService2, aboutUsService3 };
--- src/pages/about_us/sections/about_us_appointment/about_us_appointment_section.tsx
+++ src/pages/about_us/sections/about_us_appointment/about_us_appointment_section.tsx
@@ -1,9 +1,69 @@
-import './about_us_appointment_section.css';
+import "./about_us_appointment_section.css";
+import AboutUsIcons from "../../../../assets/icons/about_us/index";
+import SharedComponenets from "../../../../shared/components/index";
 
 const AboutUsAppointmentSection = () => {
   return (
-    <section id='about-us-appointment'>AboutUsAppointmentSection</section>
-  )
-}
+    <section id="about-us-appointment">
+      <div className="wrapper">
+        <div className="content-container">
+          <div className="request-appointment">
+            <SharedComponenets.AboutSectionTitle text="Appointment" />
+            <div className="title">Request a Quote</div>
+            <div className="content-container">
+              <input
+                type="text"
+                placeholder="Full Name"
+                className="appointment-text-field"
+              />
+              <input
+                type="text"
+                placeholder="E - Mail"
+                className="appointment-text-field"
+              />
+            </div>
+            <div className="content-container">
+              <input
+                type="text"
+                placeholder="Phone Number"
+                className="appointment-text-field"
+              />
+              <input
+                type="text"
+                placeholder="Select Service"
+                className="appointment-text-field"
+              />
+            </div>
+            <textarea
+              placeholder="Message"
+              className="appointment-multiline-text-field"
+            />
+            <SharedComponenets.CustomButton
+              text="Appointment"
+              onClick={() => {}}
+            />
+          </div>
+          <div className="about-us-service">
+            <SharedComponenets.ServiceInfoCard
+              image={AboutUsIcons.aboutUsService1}
+              title="High Quality Build"
+              subtitle="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+            />
+            <SharedComponenets.ServiceInfoCard
+              image={AboutUsIcons.aboutUsService2}
+              title="High Quality Build"
+              subtitle="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+            />
+            <SharedComponenets.ServiceInfoCard
+              image={AboutUsIcons.aboutUsService3}
+              title="High Quality Build"
+              subtitle="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default AboutUsAppointmentSection
+export default AboutUsAppointmentSection;
