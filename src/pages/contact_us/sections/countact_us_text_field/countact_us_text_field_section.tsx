0cc53d986ae31ebd3efd7f99cf0550d51c697e9b
MohamedAshiq11 <ashiqap007@gmail.com>
2024-03-10 22:50:45 +0400

    feat: contact us text field added

--- src/pages/contact_us/sections/countact_us_text_field/countact_us_text_field_section.tsx
+++ src/pages/contact_us/sections/countact_us_text_field/countact_us_text_field_section.tsx
@@ -1,9 +1,43 @@
 import "./countact_us_text_field_section.css";
+import SharedComponenets from "../../../../shared/components/index";
 
 const CountactUsTextFieldSection = () => {
   return (
     <section id="countact-us-text-field">
-      <div className="wrapper">CountactUsTextField</div>
+      <div className="wrapper">
+        <div className="content-container">
+          <input
+            type="text"
+            className="text-field"
+            placeholder="Your Full Name"
+          />
+          <input
+            type="text"
+            className="text-field"
+            placeholder="Your Email Address"
+          />
+        </div>
+        <div className="content-container">
+          <input
+            type="text"
+            className="text-field"
+            placeholder="Your Phone Number"
+          />
+          <input
+            type="text"
+            className="text-field"
+            placeholder="Your Address"
+          />
+        </div>
+        <textarea
+          className="multiline-text-field"
+          placeholder="Enter Your Message Here..."
+        />
+        <SharedComponenets.CustomButton
+          text="Submit Message"
+          onClick={() => {}}
+        />
+      </div>
     </section>
   );
 };
